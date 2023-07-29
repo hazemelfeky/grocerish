b8252705783f461d6e5a85430936ca1a0f29e3c5
hazemelfeky <hazemelfeky25@gmail.com>
2023-07-29 15:25:51 +0300

    feat: add banners sections

---
+++ src/components/Sections/SectionsBanners.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="banners wrapper">
+    <div class="banners__delivery">
+      <span class="banners__delivery__label label">Free Delivery</span>
+      <h4 class="banners__delivery__offer">Free delivery over 350 EGP</h4>
+      <p class="banners__delivery__comment">
+        Don’t mess a need. Free delivery!
+      </p>
+      <a class="banners__delivery__shopnow shopnow" href="">Shop Here</a>
+      <img class="banners__delivery__image" alt="delivery" src="/src/assets/Offer1.png" />
+    </div>
+    <div class="banners__deals">
+      <span class="banners__deals__label label">Best Deals</span>
+      <h4 class="banners__deals__offer">Organic Vegetables</h4>
+      <p class="banners__deals__comment">
+        Save up to 60% off on your first order.
+      </p>
+      <a class="banners__deals__shopnow shopnow" href="">Shop Here</a>
+      <img class="banners__deals__image" alt="deals" src="/src/assets/Offer2.png" />
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>

2f70769279c98aa4e8850a2282ad9fd17bdde92b
hazemelfeky <hazemelfeky25@gmail.com>
2023-09-19 00:12:56 +0300

    feat: a styling trick to place slide in routerLink

--- src/components/Sections/SectionsOffers.vue
+++ src/components/Sections/SectionsOffers.vue
@@ -1,19 +1,11 @@
 <script setup>
-import AppCardsSlider from "../App/AppCardsSlider.vue";
-import AppCard from "../App/AppCard.vue";
+import AppCardsSlider from "@/components/App/AppCardsSlider.vue";
 import { SwiperSlide } from "swiper/vue";
-import { watch } from "vue";
-import useVegetablesStore from "../../plugins/vegetablesStore";
+import useVegetablesStore from "@/plugins/vegetablesStore";
 
 const NUMBER_OF_SLIDES = 8;
 const store = useVegetablesStore();
-const products = ref([]);
-watch(
-  () => store.allProducts,
-  () => {
-    products.value = store.getRandomProducts(NUMBER_OF_SLIDES);
-  }
-);
+const products = computed(() => store.getRandomProducts(NUMBER_OF_SLIDES));
 </script>
 <template>
   <AppCardsSlider
@@ -27,24 +19,27 @@ watch(
       :key="product.id"
       class="offers__slide"
     >
-      <img
-        class="offers__slide__img"
-        :src="`${IMAGES_BASE_URL}${product.family}.png`"
-        alt="img"
-      />
-      <div class="offers__slide__type">
-        <p>{{ product.family }}</p>
-        <p>{{ product.genus }}</p>
-      </div>
-      <h3 class="offers__slide__name">{{ product.name }}</h3>
-      <div class="offers__slide__price">
-        <h3>{{ product.price.toFixed(2) }} EGP</h3>
-        <p>{{ (product.price * 1.25).toFixed(2) }} EGP</p>
-      </div>
-      <button class="offers__slide__add">
-        <Icon icon="bx:cart-add" />
-        Add To Cart
-      </button>
+      <router-link :to="`/product/${product.id}`">
+        <img
+          class="offers__slide__img"
+          :src="`${IMAGES_BASE_URL}${product.family}.png`"
+          alt="img"
+        />
+        <div class="offers__slide__type">
+          <p>{{ product.family }}</p>
+          <p>{{ product.genus }}</p>
+        </div>
+
+        <h3 class="offers__slide__name">{{ product.name }}</h3>
+        <div class="offers__slide__price">
+          <h3>{{ product.price.toFixed(2) }} EGP</h3>
+          <p>{{ (product.price * 1.25).toFixed(2) }} EGP</p>
+        </div>
+        <button class="offers__slide__add">
+          <Icon icon="bx:cart-add" />
+          Add To Cart
+        </button>
+      </router-link>
     </swiper-slide>
   </AppCardsSlider>
 </template>

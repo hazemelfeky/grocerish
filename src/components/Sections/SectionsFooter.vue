9b9cd3e2223487da0f99ea49b4d90e70a03a9d39
hazemelfeky <hazemelfeky25@gmail.com>
2023-09-23 15:30:58 +0300

    feat: add footer

---
+++ src/components/Sections/SectionsFooter.vue
@@ -0,0 +1,48 @@
+<template>
+  <footer class="footer wrapper">
+    <div class="footer__top">
+      <div class="footer__top__col">
+        <h3 class="logo">Grogeyish</h3>
+        <p>
+          <span><Icon icon="mdi:location" /> Address: </span>
+          1762 School House Road
+        </p>
+        <p>
+          <span><Icon icon="mdi:call-outline" /> Call us: </span>
+          1233-777
+        </p>
+        <p>
+          <span><Icon icon="mdi:clock-outline" /> Work Hours: </span>
+          8:00 AM - 12:00 PM
+        </p>
+      </div>
+      <div class="footer__top__col">
+        <h4>Account</h4>
+        <p>Wishlist</p>
+        <p>Cart</p>
+      </div>
+      <div class="footer__top__col">
+        <h4>Useful links</h4>
+        <p>Wishlist</p>
+        <p>Cart</p>
+      </div>
+      <div class="footer__top__col">
+        <h4>Help center</h4>
+        <p>Wishlist</p>
+        <p>Cart</p>
+      </div>
+    </div>
+    <div class="footer__bottom">
+      <div class="footer__bottom__rights">© 2022, All rights reserved</div>
+      <div class="footer__bottom__links">
+        <a><Icon icon="mdi:facebook" /></a>
+        <a><Icon icon="mdi:twitter" /></a>
+        <a><Icon icon="mdi:linkedin" /></a>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -13,5 +13,6 @@ const toggleNav = () => {
     <SectionsBanners />
     <SectionsOffers />
     <SectionsAdvantages />
+    <SectionsFooter />
   </div>
 </template>

53e256eb5141125707f08472ad354a3752b1755a
hazemelfeky <hazemelfeky25@gmail.com>
2023-09-25 22:56:47 +0300

    style: fix bugs

--- src/components/Sections/SectionsBanners.vue
+++ src/components/Sections/SectionsBanners.vue
@@ -3,7 +3,7 @@
     <div class="banners__delivery">
       <span class="banners__delivery__label label">Free Delivery</span>
       <h4 class="banners__delivery__offer">Free delivery over 350 EGP</h4>
-      <p class="banners__delivery__comment">
+      <p class="banners__delivery__comment comment">
         Don’t mess a need. Free delivery!
       </p>
       <router-link class="banners__delivery__shopnow shopnow" to="/products">Shop Here</router-link>
@@ -12,7 +12,7 @@
     <div class="banners__deals">
       <span class="banners__deals__label label">Best Deals</span>
       <h4 class="banners__deals__offer">Organic Vegetables</h4>
-      <p class="banners__deals__comment">
+      <p class="banners__deals__comment comment">
         Save up to 60% off on your first order.
       </p>
       <router-link class="banners__deals__shopnow shopnow" to="/products">Shop Here</router-link>

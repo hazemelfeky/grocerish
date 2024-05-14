8ff8019995c56b9a392476ece1dc7c9c1ae82db1
hazemelfeky <hazemelfeky25@gmail.com>
2024-05-14 23:38:48 +0300

    try

--- src/plugins/router.js
+++ src/plugins/router.js
@@ -1,10 +1,13 @@
 import { createRouter, createWebHistory } from "vue-router";
+import Products from "@/views/Products/index.vue";
+import Product from "@/views/Products/_id.vue";
 
 const routes = [
   { path: "/", component: () => import("@/views/Home.vue") },
   { path: "/about", component: () => import("@/views/About.vue") },
   { path: "/categories", component: () => import("@/views/Categories.vue") },
-  
+  { path: "/products", component: Products },
+  { path: "/products/:id", component: Product },
   { path: "/cart", component: () => import("@/views/Cart.vue") },
   {
     path: "/login",
